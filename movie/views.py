16c6f1e79968ec5f57f78b5b47502e538acd0230
Juan Jara <jaracalle2041@gmail.com>
2024-08-01 09:38:55 -0500

    changes at home function

--- movie/views.py
+++ movie/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "home.html", {'name': 'Casimiro Buenavista'})
+    return render(request, "home.html", {'name': 'Juan José Jara Calle'})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
